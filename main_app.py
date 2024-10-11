ffef89e3b4eb68d05fd27a16975a39a800b9346c
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-10-11 18:50:22 -0500

    corrigiendo predict primer modelo

--- main_app.py
+++ main_app.py
@@ -63,7 +63,7 @@ if upload_file is not None:
         X_train, X_test, y_train, y_test = train_test_split(X[selected_features], y, test_size=0.2, random_state=42)
         model = RandomForestClassifier()
         model.fit(X_train, y_train)
-        y_pred = model.predict(y_test)
+        y_pred = model.predict(X_test)
 
         st.write('### Metrica de Evaluacion para el Modelo de Importancias')
         st.write(classification_report(y_test, y_pred))
