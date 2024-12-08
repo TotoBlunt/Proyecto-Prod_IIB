ed241e6530e7cc7d8d719d6c3dc098d3f99ae58c
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-08 18:01:04 -0500

    prob

--- main_v2.py
+++ main_v2.py
@@ -8,6 +8,6 @@ st.title("Proyecto Productivo para la prediccion del peso de pollos usando varia
 df = subir_archivo()
 top5 = seleccion_variables(df)
 modelo,y_pred_model,y_test_model,x_train_model,y_train_model = modelo_ensemble(top5,df)
-data_insert = menu_opciones(modelo,y_pred_model,y_test_model,df,x_train_model,y_train_model)
+menu_opciones(modelo,y_pred_model,y_test_model,df,x_train_model,y_train_model)
 
 
