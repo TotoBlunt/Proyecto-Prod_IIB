c18366bd6e569fab217ccec3f9d8f349cb946922
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-08 14:27:14 -0500

    utilizando archivos toml

--- supabase_connector.py
+++ supabase_connector.py
@@ -1,17 +1,23 @@
 from supabase import create_client
+import toml
 import os
 
 #Se realizo al creacion de secrets en github para la gestion de credenciales de supabase
-def get_supabase_credentials():
+'''def get_supabase_credentials():
     """Obtiene las credenciales de Supabase desde las variables de entorno."""
     supabase_url = os.getenv('SUPABASE_URL')
     supabase_key = os.getenv('SUPABASE_KEY')
     if not supabase_url or not supabase_key:
         raise ValueError("Las credenciales de Supabase no están configuradas.")
 
-    return supabase_url, supabase_key
+    return supabase_url, supabase_key'''
 
-supabase_url, supabase_key = get_supabase_credentials()
+def cargar_credenciales():
+    with open('config.toml', 'r') as f:
+        config = toml.load(f)
+    return config['supabase']['url'], config['supabase']['key']
+
+supabase_url, supabase_key = cargar_credenciales()
 #obtener credenciales
 
 def crear_cliente(supabase_url, supabase_key):
