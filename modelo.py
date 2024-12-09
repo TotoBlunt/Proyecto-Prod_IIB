c8a324fbc6a3332ddfd91bb05ad4f0839f7596e1
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-09 00:10:15 -0500

    po

--- modelo.py
+++ modelo.py
@@ -294,16 +294,16 @@ def menu_opciones(modelo, y_pred_model, y_test_model, df, x_train_model, y_train
                 "feature_3": feature_3,
                 "feature_4": feature_4,
                 "feature_5": feature_5,
-                "created_at": 'created_at',
+                "created_at":created_at,
                 "prediction": prediction
             }
             ]
             
-            data_to = json.dumps(data,indent=4)
-            st.json(data_to)
+            
+            st.json(data)
                         
             if st.button('Guardar Datos'):
-                sup= crear_prediccion(data_to)
+                sup= crear_prediccion(data)
                 st.success('Guardado')
                 
 #Estilos
