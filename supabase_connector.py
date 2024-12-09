63f5a332369c5e1d4a26a977865667ab62e215de
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-08 22:58:56 -0500

    prob

--- supabase_connector.py
+++ supabase_connector.py
@@ -4,14 +4,12 @@ import os
 import streamlit as st
 #Se realizo al creacion de secrets en github y streamlit para la gestion de credenciales de supabase
 def inicializar_supabase():
-    try:
-        """Obtiene las credenciales de Supabase desde las variables de entorno."""
-        supabase_url = os.getenv('SUPABASE_URL')
-        supabase_key = os.getenv('SUPABASE_KEY')
-        client = create_client(url, key)
-        return client
-    except Exception as e:
-        st.error("Detalles del error:", str(e))  # Registro adicional
-        raise
+    
+    """Obtiene las credenciales de Supabase desde las variables de entorno."""
+    supabase_url = os.getenv('SUPABASE_URL')
+    supabase_key = os.getenv('SUPABASE_KEY')
+    client = create_client(url, key)
+    return client
+    
 
 
