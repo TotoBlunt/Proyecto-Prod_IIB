83cfe87b1408cfc8db27ce521375832557bb385c
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-08 20:10:18 -0500

    prob

--- supabase_connector.py
+++ supabase_connector.py
@@ -3,7 +3,7 @@ import toml
 import os
 
 #Se realizo al creacion de secrets en github y streamlit para la gestion de credenciales de supabase
-def get_supabase_credentials():
+def inicializador_supabase():
     """Obtiene las credenciales de Supabase desde las variables de entorno."""
     supabase_url = os.getenv('SUPABASE_URL')
     supabase_key = os.getenv('SUPABASE_KEY')
