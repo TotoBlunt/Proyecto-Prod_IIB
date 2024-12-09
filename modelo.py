0b7b3cf99f2ff79dd03c0e4513ab3de61082f43f
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-09 00:35:58 -0500

    p

--- modelo.py
+++ modelo.py
@@ -216,7 +216,8 @@ def menu_opciones(modelo,y_pred_model,y_test_model,df,x_train_model,y_train_mode
                     'created_at':created_at,
                     'prediction':prediction
                 }
-            data_to_insert =json.dumps(data,indent=4)
+            data_to_insert =[json.dumps(data,indent=4)]
+            st.write(data_to_insert)
             if st.button('Guardar Datos'):
                 crear_prediccion(data_to_insert)
                 st.success('Guardado')
