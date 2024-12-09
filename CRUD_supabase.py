02ae43c38d8d94875ac6dfbe2a1aaae80929f0c8
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-08 23:07:03 -0500

    pro

--- CRUD_supabase.py
+++ CRUD_supabase.py
@@ -7,23 +7,11 @@ client = inicializar_supabase()
 
 def crear_prediccion(predicction_data):
     st.subheader('Ingresar registro')
-    try:
+ 
         # Insertar datos en Supabase
-        response = client.table('datos_predicciones').insert(predicction_data).execute()
-        
-        # Mostrar la respuesta completa para depuración
-        st.write("Respuesta de Supabase:", response)
-
-        # Verificar estado de la respuesta
-        if response.data:
-            st.success('Registro creado con éxito')
-        elif hasattr(response, 'status_code') and response.status_code != 201:
-            st.error(f"Error al crear el registro: {response.get('message', 'Mensaje desconocido')}")
-        else:
-            st.error("No se recibió una respuesta válida del servidor.")
-    
-    except Exception as e:
-        st.error(f"Error inesperado: {e}")
+    response = client.table('datos_predicciones').insert(predicction_data).execute()
+    return response   
+   
     
 
 def read_prediccion():
