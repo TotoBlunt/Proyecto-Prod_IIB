cc0c525f86aca6390331be2635dc2fdd9469e999
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-16 20:36:22 -0500

    edit

--- modelo.py
+++ modelo.py
@@ -201,8 +201,7 @@ def menu_opciones(modelo, y_pred_model, y_test_model, df, x_train_model, y_train
         feature_5 = float(st.number_input('Ingresa el valor para MortStd', format="%.3f"))
         created_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')  # Formato más limpio
         feature_6 = str(uuid.uuid4())
-        #Diccionario
-        datos = {'id':feature_6,'peso_sem4':feature_1,'agua':feature_2,'peso_sem3':feature_3,'consumo_acabado':feature_4,'mortalidad_std':feature_5}
+        
         # Botón para realizar la predicción
         if st.button('Realizar Predicción'):
             # Validar que las entradas no estén vacías
