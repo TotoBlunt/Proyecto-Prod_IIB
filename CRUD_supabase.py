561333be8cb0c4ffdbcff77655b250fe9194ce02
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-16 21:38:38 -0500

    edit

--- CRUD_supabase.py
+++ CRUD_supabase.py
@@ -14,9 +14,10 @@ def crear_prediccion(predicction_data):
     try:
         # Mostrar los datos que se van a insertar (opcional)
         st.write("Datos a insertar:", predicction_data)
-        #predicction_data = json.dumps(predicction_data,indent=4)
+        predicction_data = json.dumps(predicction_data,indent=4)
+        data = json.loads(predicction_data)
         # Insertar datos en Supabase
-        response = Client.table('datos_predicciones').insert(predicction_data).execute()
+        response = Client.table('datos_predicciones').insert(data).execute()
 
         # Mostrar la respuesta completa para depuración (opcional)
         st.write("Respuesta de Supabase:", response)
