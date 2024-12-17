cd6801cff9c75e1e3944551d7bf0f0eb1789b5e0
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-16 21:54:49 -0500

    edit

--- main_v2.py
+++ main_v2.py
@@ -7,7 +7,39 @@ aplicar_estilos()
 #Titulo para el app
 st.title("Proyecto Productivo para la prediccion del peso de pollos usando variables descritas por el modelo SelectcKbest luego hacer las predicciones usando el Modelo Ensemble, con Streamlit(v2)")
 
+# Cargar archivo
+df = subir_archivo()
 
+if df is not None:
+    # Seleccionar las mejores variables
+    top5 = seleccion_variables(df)
+    if top5:
+        # Entrenar el modelo ensemble
+        modelo, y_pred_model, y_test_model, x_train_model, y_train_model = modelo_ensemble(top5, df)
+
+        # Menú de opciones para interactuar con el modelo
+        input_data, datos = menu_opciones(modelo, y_pred_model, y_test_model, df, x_train_model, y_train_model)
+
+        # Botón para realizar predicción
+        if st.button('Realizar Predicción'):
+            datos_edit = prediccion(modelo, input_data, datos)
+
+            # Mostrar datos predichos antes de guardar
+            st.write("### Resultado de la predicción:")
+            st.write(datos_edit)
+
+            # Botón para guardar los datos en Supabase
+            if st.button('Guardar'):
+                try:
+                    crear_prediccion(datos_edit)
+                    st.success('Datos guardados correctamente en Supabase.')
+                except Exception as e:
+                    st.error(f"Error al guardar los datos: {e}")
+
+else:
+    st.write("No se ha cargado ningún archivo.")
+
+'''
 df = subir_archivo()
 
 if df is not None:
@@ -25,4 +57,4 @@ else:
     st.write("No se ha cargado ningún archivo.")
 
 
-
+'''
--- modelo.py
+++ modelo.py
@@ -220,7 +220,7 @@ def menu_opciones(modelo, y_pred_model, y_test_model, df, x_train_model, y_train
                 }
     else:
         st.error("### Por favor, ingresa valores válidos para todas las características.")
-    st.write(datos)
+    #st.write(datos)
     return input_data,datos
         
 def prediccion(modelo,input_data,datos):
