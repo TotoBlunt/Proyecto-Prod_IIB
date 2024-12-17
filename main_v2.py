f3d866df1547c628137a3f7286c28f135d8a1b21
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-16 21:10:12 -0500

    edit

--- main_v2.py
+++ main_v2.py
@@ -15,9 +15,10 @@ if df is not None:
     if top5:
         modelo,y_pred_model,y_test_model,x_train_model,y_train_model = modelo_ensemble(top5,df)
         datos = menu_opciones(modelo,y_pred_model,y_test_model,df,x_train_model,y_train_model)
-        if st.button('Guardar'):
-            crear_prediccion(datos)
-            st.success('Datos guardados correctamente')
+        st.write(datos)
+        #if st.button('Guardar'):
+         #   crear_prediccion(datos)
+          #  st.success('Datos guardados correctamente')
 
 else:
     st.write("No se ha cargado ningún archivo.")
