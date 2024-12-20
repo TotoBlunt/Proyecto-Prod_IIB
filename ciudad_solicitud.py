4363c7cbf601cf96884efe0072b2f4c3a5caf05c
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-19 22:09:48 -0500

    update

--- ciudad_solicitud.py
+++ ciudad_solicitud.py
@@ -7,10 +7,21 @@ def obtener_IPpublica():
     ip_public = urllib.request.urlopen('https://ident.me').read().decode('utf8')
     return ip_public
 
-def geolocalizar_ip(ip):
+def geolocalizar_ip():
     # URL de la API de ipinfo.io con la IP publica específica
     url = f"https://ipinfo.io/{ip}/json"
-    st.write(ip)
+
+    # Código JavaScript para obtener la ubicación
+    js_code = """
+    fetch('https://ipinfo.io/json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('location').innerText = `Tu ubicación es: ${data.city}, ${data.country}`;
+    });
+    """
+
+    # Mostrar la ubicación en Streamlit
+    st.write(f'<div id="location">Obteniendo ubicación...</div><script>{js_code}</script>', unsafe_allow_html=True)
     try:
         # Realizar la solicitud HTTP GET
         respuesta = requests.get(url)
