4b815e7729c192ea78ba9cce6d6c88884fa4e8c1
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-12-19 22:48:27 -0500

    update

--- CRUD_supabase.py
+++ CRUD_supabase.py
@@ -10,10 +10,10 @@ Client = inicializar_supabase()
 
 
 def crear_prediccion(predicction_data):
-    st.subheader('Ingresar registro')
+    st.subheader('Ingresando registro...')
     try:
         # Mostrar los datos que se van a insertar (opcional)
-        st.write("## Datos a insertar:", predicction_data)
+        st.write("## Datos a insertar:", json.dumps(predicction_data,indent=4))
 
         # Convertir el diccionario a JSON
         json_data = json.loads(predicction_data)  # json.dumps crea el formato correcto
