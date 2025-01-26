db8e375df22e26c69331e7f50b91f040470a34ad
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2025-01-26 16:30:56 -0500

    update

--- main_v2.py
+++ main_v2.py
@@ -76,8 +76,8 @@ with col2:
                     # Mostrar la lista y el campo de ID solo si se ha pulsado "Eliminar"
                     if st.session_state['mostrar_lista_y_campo_id']:
                         # Mostrar la lista de predicciones
-                        st.write("Lista de predicciones:")
-                        listar_registros()
+                        st.write("### Lista de predicciones:")
+                        registros = listar_registros()  # Suponiendo que listar_registros() devuelve los registros
 
                         # Campo para ingresar el ID a eliminar
                         prediccion_id = st.number_input("Ingresa el ID del registro que deseas eliminar:", min_value=0)
