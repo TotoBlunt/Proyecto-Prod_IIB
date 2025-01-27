43ce3d857ace2c61eb2e5dbdee0a739842eb0357
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2025-01-26 19:44:29 -0500

    update

--- main_v2.py
+++ main_v2.py
@@ -19,7 +19,7 @@ with col1:
    #url_imagen_1 = "https://i.imgur.com/ZX7HTol.jpeg"  # Ejemplo de imagen pública
    #st.image(url_imagen_1, caption="Integrante Numero 1", use_container_width=True)
     st.write("- Jhon Max Lozano Sulluchuco")
-    st.write("- Jhon Max Lozano Sulluchuco")
+    st.write("- Mattew Deni Mamani Flores")
     st.write("- Elvis Alberto Portilla Flores")
     st.write("- Jose Antonio Longa Mendoza")
 
